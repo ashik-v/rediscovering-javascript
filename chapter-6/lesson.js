ea9782b69399aa2e4f2241a70cacbc3c331920d1
Ashik Varghese <ashikv@nulogy.com>
2022-05-11 21:59:49 -0400

    chapter-6: Complete lesson

--- chapter-6/lesson.js
+++ chapter-6/lesson.js
@@ -74,7 +74,7 @@ const createPerson = (name, age, sport, sportFn) => {
     toString: function() {
       return `${this.name} ${this.age}`;
     },
-  [`play${sport}`]: sportFn //use template literal instad of person['play' + sport] and have the computed propetry in the returned object
+  [`play${sport}`]: sportFn //use template literal instad of person['play' + sport] and have the computed property in the returned object
   };
 };
 
@@ -103,15 +103,56 @@ const[, , ,bar = 'baz'] = getPresidentName(); //using a default value when one i
 console.log(bar);
 const[first4, ...rest] = getPresidentName(); //rest extraction
 console.log(first4, rest);
+const [test = false] = []; //default value at time of destructuring - not very usefuly for array destructuring though
+console.log(test)
 
 let [a, b] = [1, 2];
 [a, b] = [b, a]; //easy way to swap values
 console.log(a, b);
+
+let c = 3, d = 4;
+[c, d] = [d, c] //swapping works like this too
+console.log(c, d);
 
 const printFirstAndLastOnly = function([first, , last]) { //destructuring in parameter list
   console.log(`${first} ${last}`);
 };
 printFirstAndLastOnly(['John', 'Q', 'Adams']);
 
-//object destructuring
+console.log('**********');
 
+//object destructuring
+const weight = 'WeightKG';
+
+const john = {
+  theName: 'john',
+  theAge: 2,
+  height: 110,
+  address: { street: '404 Missing Street' },
+  shipping: { street: '500 NoName St'},
+  [weight]: 15,
+  [Symbol.for('favoriteColor')]: 'Orange',
+}
+
+const { theName: theDestructuredName, theAge: theDestructuredAge } = john;
+console.log(theDestructuredName, theDestructuredAge);
+const { theName, theAge } = john; //no need to specify the object literal's key if the destructured variable name matches it
+console.log(theName, theAge);
+const { [weight]: wt, [Symbol.for('favoriteColor')]: favColor } = john; //use [] to destructure computed properties
+console.log(wt, favColor);
+const { lat, lon, fav = true } = { lat: 45.33, lon: 32.00 }; //assign default value at time of destructuring
+console.log(lat, lon, fav);
+
+const printInfo = ({theName, theAge}) => console.log(`This person is called ${theName} and is ${theAge} years old.`) //can destructure in parameter list directly which means no need to pass the whole object
+printInfo(john);
+
+const { address: { street }, shipping: { street: theStreet } } = john; //deep destructuring and avoinding conflicts for similarly named deep properties
+console.log(street, theStreet);
+
+let thaName = 'foo';
+({ theName: thaName } = john); //need to wrap in () if reusing a previous variable
+console.log(thaName);
+
+const addNewProp = (person) => ({...person, gotcha: 'sucker'}) //when adding(extending) objects always prefer to copy the whole object
+newJohn = addNewProp(john);
+console.log(newJohn);
