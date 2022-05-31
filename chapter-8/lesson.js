9c42a08c40d62e36b1ff18f8980d1ccd4e3202c5
Ashik Varghese <ashikv@nulogy.com>
2022-05-30 22:16:18 -0400

    Complete lesson

--- chapter-8/lesson.js
+++ chapter-8/lesson.js
@@ -2,6 +2,8 @@
 // this uses object chain to delegate calls -> instead of relying on a base class, rely on the next object in the chain
 // base class is called the objects prototype
 
+'use strict'
+
 class Counter {}
 const counter1 = new Counter;
 const counter2 = new Counter;
@@ -89,9 +91,92 @@ class ComputerWiz {}
 Object.setPrototypeOf(Object.getPrototypeOf(alan), ComputerWiz.prototype);
 printPrototypeHeirarchy(alan);
 console.log('**********');
-ada = new ReputablePerson('ada', 'lovalace', 5);
+const ada = new ReputablePerson('ada', 'lovalace', 5);
 console.log('**********');
 printPrototypeHeirarchy(ada); // ada also has the modified prototype chain
+
+// default constructors do not need a specific super call
+class AwesomePerson extends Person {
+  get fullName() { return `Awesome ${super.fullName}`; } // the base constructor is automatically called
+}
+
+const lebron = new AwesomePerson('LeBron', 'James');
+console.log(lebron);
+
+console.log('**********');
+
+// new extends keyword is compatible with legacy classes as well
+function LegacyClass(value) {
+  this.value =  value;
+}
+
+class NewClass extends LegacyClass { }
+
+console.log(new NewClass(1));
+
+console.log('**********');
+
+// instance types with species
+class MyString extends String {}
+class MyArray extends Array {}
+
+const concString = new MyString().concat(new MyString); // why do I need the parenthesis in front of MyString
+const concArray = new MyArray().concat(new MyArray);
+
+console.log(`instance of MyString: ${concString instanceof MyString}`); // string responds with the base class
+console.log(`instance of MyArray: ${concArray instanceof MyArray}`); // while Array doesn't
+
+console.log('**********');
+
+// sticking to the base class
+class Names {
+  constructor(...names) {
+    this.names = names;
+  }
+
+  filter1(selector) {
+    return new Names(...this.names.filter(selector)); // why do I need to spread this.names - isn't it already an array? -> spreading is on the result of the this.names.filter
+  }
+
+  filter2(selector) {
+    const constructor = Object.getPrototypeOf(this).constructor; // .constructor returns a reference to the object constructor function that created the instance, i.e. SpecializedNames in this case - why not just use this? Point of Object.getPrototypeOf?
+    return new constructor(...this.names.filter(selector));
+  }
+
+  filter3(selector) {
+    const constructor = this.constructor.kindHint || this.constructor;
+    return new constructor(...this.names.filter(selector));
+  }
+}
+
+class SpecializedNames extends Names {
+  static get kindHint() {
+    return Names;
+  }
+}
+
+const specializedNames = new SpecializedNames('Java', 'C++', 'JavaScript');
+const baseNames = new Names('Java', 'C++', 'JavaScript');
+console.log(specializedNames.filter1(name => name.startsWith('Java'))); // filter1 returns a new Names object and hence sticks to the base class
+console.log(baseNames.filter1(name => name.startsWith('Java')));
+console.log('**********');
+// choosing at runtime
+console.log(specializedNames.filter2(name => name.startsWith('Java')));
+console.log(baseNames.filter2(name => name.startsWith('Java')));
+
+console.log(new alan.constructor('ashik', 'var', 5)); // one way to use the constructor method
+
+// configuring the type
+console.log(specializedNames.filter3(name => name.startsWith('Java'))); // looks at the static getter to get the class
+
+// Symbol.species()
+class MyOtherArray extends Array {
+  static get [Symbol.species]() { // use Symbol.species instead of a random getter name like kindHint
+    return Array;
+  }
+}
+const otherConcArray = new MyOtherArray().concat(new MyOtherArray);
+console.log(`instance of Array: ${otherConcArray instanceof Array}`);
 
 
 
