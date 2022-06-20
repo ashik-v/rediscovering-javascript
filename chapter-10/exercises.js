6c8a409b56e023c0e127cad11e3e10b13d681756
Ashik Varghese <ashikv@nulogy.com>
2022-06-19 21:42:53 -0400

    Complete exercise

--- chapter-10/exercises.js
+++ chapter-10/exercises.js
@@ -2,4 +2,4 @@
 // Promise.race() will wait for just one promise to resolve before calling it
 
 // exercise 2
-// yes, Promise.all() will waut for all promises to either resolve or be rejected
+// yes, Promise.all() will wait for all promises to either resolve or be rejected
